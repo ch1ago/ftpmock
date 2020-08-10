babc5f41085d6147839a035c6cec0e4c92a8ecd5
James Pinto <thejamespinto@gmail.com>
2020-08-09 20:19:31 -0400

    Ruby loading - Replacing preload with autoload

--- lib/ftpmock.rb
+++ lib/ftpmock.rb
@@ -1,15 +1,29 @@
 require 'pathname'
-require 'ftpmock/version'
 
-Dir['lib/ftpmock/{utils,core,helpers,proxies}/*.rb'].each do |f|
-  load f
-end
+require 'ftpmock/configuration'
+require 'ftpmock/version'
 
 module Ftpmock
   class Error < StandardError; end
   class GetNotFetched < Error; end
   class PutFileNotFound < Error; end
   class PutLocalDiffersFromCache < Error; end
+
+  autoload :Cache, 'ftpmock/core/cache'
+  # autoload :Configuration, 'ftpmock/core/configuration'
+
+  autoload :GetHelper,  'ftpmock/helpers/get_helper'
+  autoload :ListHelper, 'ftpmock/helpers/list_helper'
+  autoload :PathHelper, 'ftpmock/helpers/path_helper'
+  autoload :PutHelper,  'ftpmock/helpers/put_helper'
+
+  autoload :NetFtpProxy, 'ftpmock/proxies/net_ftp_proxy'
+  autoload :NetSftpProxy, 'ftpmock/proxies/net_sftp_proxy'
+  autoload :MethodMissingMixin, 'ftpmock/proxies/method_missing_mixin'
+
+  autoload :ColorUtils, 'ftpmock/utils/color_utils'
+  autoload :StringUtils, 'ftpmock/utils/string_utils'
+  autoload :VerboseUtils, 'ftpmock/utils/verbose_utils'
 
   def self.on!(&block)
     NetFtpProxy.on! do
